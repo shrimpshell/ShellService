a66defa115e174b213c5022486c504f4fcaa56a3
lopin010109 <halu_9@hotmail.com>
2018-09-16 20:45:52 +0800

    no message

--- src/server/ws/WsServer.java
+++ src/server/ws/WsServer.java
@@ -1,17 +1,14 @@
 package server.ws;
 
-<<<<<<< HEAD
+
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.*;
 import java.io.*;
 
 import javax.websocket.CloseReason;
-=======
+
 import java.io.IOException;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
->>>>>>> ebbc77328578bbf2f8600806d96821ae473ef1db
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
 import javax.websocket.OnMessage;
@@ -19,10 +16,7 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
-
 import com.google.gson.Gson;
-
-<<<<<<< HEAD
 import server.ws.ChatMessage;
 import server.ws.StateMessage;
 
@@ -35,8 +29,7 @@ public class WsServer {
 	@OnOpen
 	public void onOpen(@PathParam("userName") String userName, Session userSession) throws IOException {
 		/* save the new user in the map */
-		sessionsMap.put(userName, userSession);
-		
+		sessionsMap.put(userName, userSession);	
 		/* Sends all the connected users to the new user */
 		Set<String> userNames = sessionsMap.keySet(); // 取得所有的userID
 		StateMessage stateMessage = new StateMessage("open", userName, userNames);
@@ -45,37 +38,24 @@ public class WsServer {
 		for (Session session : sessions) {
 			if (session.isOpen()) {
 				session.getAsyncRemote().sendText(stateMessageJson);
+				}
 			}
-		}
-		
-
 		String text = String.format("Session ID = %s, connected; userName = %s%n ", userSession.getId(),
 				userName);
 		System.out.println(text);
-	}
+		}
+	
 	@OnMessage
-	public void onMessage(Session userSession, String message){
+	public void onMessage(Session userSession, String message) {
 		ChatMessage chatMessage = gson.fromJson(message, ChatMessage.class);
 		String receiver = chatMessage.getReceiver();
 		Session receiverSession = sessionsMap.get(receiver);
 		if (receiverSession != null && receiverSession.isOpen()) {
 			receiverSession.getAsyncRemote().sendText(message);
-		}
+			}
 		System.out.println("Message received: " + message);
-=======
-@ServerEndpoint("/Services/{userName}")
-public class WsServer {
-	private static Map<String, Session> sessionsMap = new ConcurrentHashMap<>();
-	Gson gson = new Gson();
-	
-	@OnOpen
-	public void onOpen(@PathParam("userName") String userName, Session userSession) throws IOException {
-		System.out.println("Open Connection ...");
->>>>>>> ebbc77328578bbf2f8600806d96821ae473ef1db
-	}
+		}
 
-		
-	
 	@OnClose
 	public void onClose(Session userSession, CloseReason reason){
 		String userNameClose = null;
@@ -85,7 +65,9 @@ public class WsServer {
 				userNameClose = userName;
 				sessionsMap.remove(userName);
 				break;
+				
 			}
+			
 		}
 
 		if (userNameClose != null) {
@@ -94,18 +76,21 @@ public class WsServer {
 			Collection<Session> sessions = sessionsMap.values();
 			for (Session session : sessions) {
 				session.getAsyncRemote().sendText(stateMessageJson);
+				
 			}
+		
 		}
 
 		String text = String.format("session ID = %s, disconnected; close code = %d%nusers: %s", userSession.getId(),
 				reason.getCloseCode().getCode(), userNames);
 		System.out.println(text);
+		
 	}
 	
-	
-
 	@OnError
 	public void onError(Throwable e){
 		System.out.println("Error: " + e.toString());
+		
 	}
+	
 }
