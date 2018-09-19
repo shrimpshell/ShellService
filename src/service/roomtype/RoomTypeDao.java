c68d7ec59c6255917bcbf5b80824087da207ecdd
tony760815 <tony760815@gmail.com>
2018-09-19 22:24:09 +0800

    新增RoomType

---
+++ src/service/roomtype/RoomTypeDao.java
@@ -0,0 +1,14 @@
+package service.roomtype;
+
+import java.util.List;
+
+import service.event.Events;
+
+public interface RoomTypeDao {
+	int insert(RoomType spot, byte[] image);
+	int update(RoomType spot, byte[] image);
+	int delete(int id);
+	byte[] getImage(int id);
+	RoomType findById(int id);
+	List<RoomType> getAll();
+}
