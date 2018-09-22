d63ced4810f3a4bf270a47caafc6b3fea0f12153
lopin010109 <halu_9@hotmail.com>
2018-09-23 04:07:56 +0800

    no message

--- src/service/instant/InstantServlet.java
+++ src/service/instant/InstantServlet.java
@@ -14,15 +14,12 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
-import service.customer.Customer;
 import service.employee.Employees;
 
 
 
-
-
 @SuppressWarnings("serial")
-@WebServlet("/InstantServlet")
+@WebServlet("InstantServlet")
 public class InstantServlet extends HttpServlet {
 	private final static String CONTENT_TYPE = "text/html; charset=utf-8";
 	InstantDao instantDao = null;
@@ -31,13 +28,11 @@ public class InstantServlet extends HttpServlet {
 		if (instantDao == null) {
 			instantDao = new InstantDaoMySqlImpl();
 		}
-		List<Instant> instants = instantDao.getAll();
-		writeText(response, new Gson().toJson(instants));
+		List<Instant> instant = instantDao.getAll();
+		writeText(response, new Gson().toJson(instant));
 	}
 	
-	
-
-	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
 		Gson gson = new Gson();
 		BufferedReader br = request.getReader();
@@ -46,39 +41,39 @@ public class InstantServlet extends HttpServlet {
 		while ((line = br.readLine()) != null) {
 			jsonIn.append(line);
 		}
-		
 		System.out.println("input: " + jsonIn);
+		
 		JsonObject jsonObject = gson.fromJson(jsonIn.toString(), JsonObject.class);
 		if (instantDao == null) {
 			instantDao = new InstantDaoMySqlImpl();
-		}
-		
-		String action = jsonObject.get("action").getAsString();
-		
-		if (action.equals("getAll")) {
-			List<Instant> events = instantDao.getAll();
-			writeText(response, gson.toJson(events));
-		
-		} else if (action.equals("instantInsert")) {
-			String instantJson = jsonObject.get("instant").getAsString();
-			Instant instant = gson.fromJson(instantJson, Instant.class);
-			writeText(response, String.valueOf(instantDao.insert(instant)));
-		
-		} else if (action.equals("instantUpdate")) {
-			Instant instant = gson.fromJson(jsonObject.get("instant").getAsString(), Instant.class);
-			writeText(response, String.valueOf(instantDao.update(instant)));
 			
+			String action = jsonObject.get("action").getAsString();
+			int count = 0;
+			if (action.equals("insertInstant")) {
+				String instantJson = jsonObject.get("instant").getAsString();
+				Instant instant = gson.fromJson(instantJson, Instant.class);
+				count = instantDao.insertInstant(instant);
+				writeText(response, String.valueOf(count));
+			} else if (action.equals("updateStatus")) {
+				String instantJson = jsonObject.get("instant").getAsString();
+				Instant instant = gson.fromJson(instantJson, Instant.class);
+				count = instantDao.updateStatus(instant);
+				writeText(response, String.valueOf(count));
+			} else if (action.equals("getStatus")) {
+				List<Instant> instants = instantDao.getAll();
+				writeText(response, gson.toJson(instants));
+			}
 		}
+		
+		
 	}
-	
-	private void writeText(HttpServletResponse response, String outText)  throws IOException {
+
+	private void writeText(HttpServletResponse response, String outText) throws IOException {
 		response.setContentType(CONTENT_TYPE);
 		PrintWriter out = response.getWriter();
 		out.print(outText);
 		System.out.println("output: " + outText);
 		
 	}
-	
-	
 
 }
