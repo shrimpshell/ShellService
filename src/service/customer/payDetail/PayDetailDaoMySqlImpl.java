53eac87d8498afaaa42fa3763e23f58b40da6c24
黃信 <tedhwang8@hotmail.com>
2018-09-26 20:21:57 +0800

    cont 語法

--- src/service/customer/payDetail/PayDetailDaoMySqlImpl.java
+++ src/service/customer/payDetail/PayDetailDaoMySqlImpl.java
@@ -95,7 +95,7 @@ public class PayDetailDaoMySqlImpl implements PayDetailDao {
 				"ON rStatus.IdRoomStatus = iDetail.IdRoomStatus " + 
 				"LEFT JOIN InstantType AS iType " + 
 				"ON iDetail.IdInstantType = iType.IdInstantType " + 
-				"WHERE rReserv.IdCustomer = 1 AND iType.InstantTypeName IS NOT NULL" + 
+				"WHERE rReserv.IdCustomer = ? " + 
 				"ORDER BY rReserv.CheckInDate ASC;";
 		Connection conn = null;
 		PreparedStatement ps = null;
@@ -158,7 +158,7 @@ public class PayDetailDaoMySqlImpl implements PayDetailDao {
 						"ON rStatus.IdRoomStatus = iDetail.IdRoomStatus " + 
 						"LEFT JOIN InstantType AS iType " + 
 						"ON iDetail.IdInstantType = iType.IdInstantType " + 
-						"WHERE rReserv.IdCustomer = 1 " + 
+						"WHERE rReserv.IdCustomer = ? " + 
 						"ORDER BY rReserv.CheckInDate ASC;";
 				ps = conn.prepareStatement(sql);
 				ps.setString(1, userId);
